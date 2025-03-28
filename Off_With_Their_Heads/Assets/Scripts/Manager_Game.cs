80b46d7d996288b744d93cdafd09667ad0721b8d
emircanalcan <alcan.emircan@gmail.com>
2025-03-28 05:32:57 +0300

    ü
    ü

--- Off_With_Their_Heads/Assets/Scripts/Manager_Game.cs
+++ Off_With_Their_Heads/Assets/Scripts/Manager_Game.cs
@@ -6,18 +6,11 @@ public class Manager_Game : MonoBehaviour
 
     #region VARIABLES
     private bool iscoop = false;
-
-    private bool ismenumain = true;
-    private bool ismenuselection = false;
-
-    private bool islevel0 = false;
-    private bool islevel1 = false;
-    private bool islevel2 = false;
     #endregion
 
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
     void Update()
     {
