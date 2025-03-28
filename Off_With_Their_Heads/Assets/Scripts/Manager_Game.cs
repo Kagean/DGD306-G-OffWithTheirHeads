96b0f5e7b174829fc2d019326b0241755238b55b
emircanalcan <alcan.emircan@gmail.com>
2025-03-28 05:15:54 +0300

    ü
    ü

---
+++ Off_With_Their_Heads/Assets/Scripts/Manager_Game.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Manager_Game : MonoBehaviour
+{
+
+    #region VARIABLES
+    private bool iscoop = false;
+
+    private bool ismenumain = true;
+    private bool ismenuselection = false;
+
+    private bool islevel0 = false;
+    private bool islevel1 = false;
+    private bool islevel2 = false;
+    #endregion
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
