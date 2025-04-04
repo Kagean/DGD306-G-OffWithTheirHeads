b7234d8ab2d0e4b59119b83cf42f9365fd8826e3
Ege Kaan <215040030@stu.istinye.edu.tr>
2025-04-04 03:42:16 +0300

    V0.2
    An introduction to basic movement, camera control, and create platforming.

---
+++ Off_With_Their_Heads/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(move, 0, 0);
+    }
+}
